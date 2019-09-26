ab0d04cb24b5dcc20657fda873cb7f95edf3bd2b
Sushil Thapa <sushilt553@gmail.com>
2019-09-26 18:18:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,10 +18,10 @@ function sayHiToGrandma(string){
     if (string.toLowerCase() === string){
       return 'I can\'t hear you!';
     }
-    if (string.toUpperCase() === string){
+    else if (string.toUpperCase() === string){
       return 'YES INDEED!';
     }
-    if (string === 'I love you, Grandma.'){
+    else if (string === 'I love you, Grandma.'){
       return 'I love you, too.';
     }
 }
