5bf286db0f850d9b2d28d8fdc1de88d9dab391d4
Sushil Thapa <sushilt553@gmail.com>
2019-09-26 18:10:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function logWhisper(string){
 
 function sayHiToGrandma(string){
   if(string==="hello"){
-  return "I can\'t hear you!";
+  return "I can't hear you!";
   }else if(string==="HELLO"){
     return "YES INDEED!";
   }
