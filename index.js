a717c53382381db2970755bc57ab7598c97b70a5
Sushil Thapa <sushilt553@gmail.com>
2019-09-26 17:56:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,3 +13,15 @@ function logShout(string){
 function logWhisper(string){
   console.log(string.toLowerCase());
 }
+
+function sayHiToGrandma(string){
+  if(string.toLowerCase()){
+  return "I can't hear you!";
+  }
+}
+
+function sayHiToGrandma(string){
+  if(string.toUpperCase()){
+  return "YES INDEED!";
+  }
+}
