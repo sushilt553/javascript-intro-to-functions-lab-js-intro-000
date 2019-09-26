9f0867c80c4d053e821eee22ffd13a27236e9419
Sushil Thapa <sushilt553@gmail.com>
2019-09-26 18:00:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,10 +17,15 @@ function logWhisper(string){
 function sayHiToGrandma(string){
   if(string.toLowerCase()){
   return "I can't hear you!";
-  }else if(string.toUpperCase()){
-  return "YES INDEED!";
-  }else if(string==="I love you, Grandma."){
-    return "I love you, too.";
   }
 }
 
+function sayHiToGrandma(string){
+  if(string.toUpperCase()){
+  return "YES INDEED!";
+  }
+}
+
+function sayHiToGrandma(string==="I love you, Grandma."){
+  
+}
